205a99ed43292a1691b502812363c0f268129eb9
Alejandro2097 <alejo12381@hotmail.es>
2021-07-10 20:16:44 -0500

    secondary

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+require('dotenv').config();
 
-
-//Crear el servidor-Aplicación de express
+// Crear el servidor/aplicación de express
 const app = express();
 
-//Rutas
-app.use( '/api/auth', require('./routes/auth'));
+// Base de datos
+dbConnection();
 
 
-app.listen(4000, () => {
-    console.log(`servidor corriendo en puerto ${ 4000 }`);
+// Directorio Público
+app.use( express.static('public') );
 
-    
+// CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+
+// Rutas
+app.use( '/api/auth', require('./routes/auth') );
+
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
+
