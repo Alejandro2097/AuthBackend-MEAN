e7a4b8cfa3c2002172eab7fd73c8d18eb10b831b
Alejandro2097 <alejo12381@hotmail.es>
2021-07-12 09:16:14 -0500

    cli

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -19,6 +20,10 @@ app.use( cors() );
 // Lectura y parseo del body
 app.use( express.json() );
 
+//maneja demas rutas
+//app.get('*' , (req ,res)=>{
+//    res.sendFile(path.resolve( __dirname, 'public/index.html'));
+//})
 
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
